6b597e58026ff995c3cf59173b592da628a9d758
Bruno Gonçalves <brunosgoncalvez@gmail.com>
2021-11-30 17:35:30 -0300

    Iniando a criação do DDD

---
+++ ARC.Domain/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARC.Domain
+{
+    public class Client
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage=" *Campo obrigatório")]
+        [MaxLength(50, ErrorMessage = "Máximo de 50 caracteres")]
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+
+    }
+}
